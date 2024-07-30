35753f767cd60a674f6f32bb4d5e200d5ed6abe6
MIR M. ALI <Mir.Ali@usda.gov>
2024-07-30 09:40:26 -0400

    Updating model to BASE2-gpt-35-turbo

--- Forest _Wizard/app.py
+++ Forest _Wizard/app.py
@@ -21,11 +21,11 @@ def make_post_request(json_body):
     
     #direct call to llm api
     #headers = {'Content-Type': 'application/json'}
-    #api_url = 'https://oai-nonprd-openai-poc-01.openai.azure.com/openai/deployments/BASE-gpt-35-turbo/chat/completions?api-version=2024-02-15-preview&api-key='+api_key
+    #api_url = 'https://oai-nonprd-openai-poc-01.openai.azure.com/openai/deployments/BASE2-gpt-35-turbo/chat/completions?api-version=2024-02-15-preview&api-key='+api_key
 
     #call to api pipeline
     headers = {'Ocp-Apim-Subscription-Key': Ocp_Apim_Subscription_Key, 'Content-Type': 'application/json'}
-    api_url = 'https://apim-com-nonprd-poc.azure-api.net/ai-pipeline/onepromt/v1/BASE-gpt-35-turbo?api-version=2024-02-15-preview'
+    api_url = 'https://apim-com-nonprd-poc.azure-api.net/ai-pipeline/onepromt/v1/BASE2-gpt-35-turbo?api-version=2024-02-15-preview'
 
     try:
         response = requests.post(api_url, headers=headers,  data=json.dumps(json_body))
