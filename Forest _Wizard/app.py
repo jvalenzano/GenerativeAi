d2362fb686a11275e5a71f0ae3880570eba3f666
MIR M. ALI <Mir.Ali@usda.gov>
2024-08-06 13:32:27 -0400

    added logic to call to openAI as an option

--- Forest _Wizard/app.py
+++ Forest _Wizard/app.py
@@ -4,13 +4,19 @@ import json
 import os
 
 app = Flask(__name__)
-api_key = os.getenv('OPENAI_API_KEY')
+api_key = 'e3632968ed2e48f5b1cac0b036b9affe'
 Ocp_Apim_Subscription_Key = '5bbf6c102d86484c818aba9d18588962'
 
-#helper function to make call to the api
-def make_post_request(json_body):
+#####################################
+#####################################
+#OpenAI APIM API call
+#####################################
+#####################################
+
+
+def make_post_request_pipeline(json_body):
     """
-    Makes a POST request to the specified API endpoint with the given JSON body.
+    Makes a POST request to the specified AI APIM endpoint with the given JSON body.
 
     Args:
         json_body (dict): The JSON body to be sent in the request.
@@ -19,11 +25,7 @@ def make_post_request(json_body):
         dict: The response JSON if the request is successful, otherwise an error dictionary with the error message and response text.
     """
     
-    #direct call to llm api
-    #headers = {'Content-Type': 'application/json'}
-    #api_url = 'https://oai-nonprd-openai-poc-01.openai.azure.com/openai/deployments/BASE2-gpt-35-turbo/chat/completions?api-version=2024-02-15-preview&api-key='+api_key
-
-    #call to api pipeline
+   
     headers = {'Ocp-Apim-Subscription-Key': Ocp_Apim_Subscription_Key, 'Content-Type': 'application/json'}
     api_url = 'https://apim-com-nonprd-poc.azure-api.net/ai-pipeline/onepromt/v1/BASE2-gpt-35-turbo?api-version=2024-02-15-preview'
 
@@ -36,23 +38,66 @@ def make_post_request(json_body):
     except Exception as e:
         return {"error": "An error occurred: " + str(e)}
 
+
+#####################################
+#####################################
+#OpenAI API call
+#####################################
+#####################################
+
+
+def make_post_request_openai(json_body):
+    """
+    Makes a POST request to the specified openAI endpoint with the given JSON body.
+
+    Args:
+        json_body (dict): The JSON body to be sent in the request.
+
+    Returns:
+        dict: The response JSON if the request is successful, otherwise an error dictionary with the error message and response text.
+    """
+    
+    headers = {'Content-Type': 'application/json'}
+    api_url = 'https://oai-nonprd-openai-poc-01.openai.azure.com/openai/deployments/BASE2-gpt-35-turbo/chat/completions?api-version=2024-02-15-preview&api-key='+api_key
+ 
+    try:
+        response = requests.post(api_url, headers=headers,  data=json.dumps(json_body))
+        if response.status_code == 200:
+            return response.json()
+        else:
+            return {"error": "Request failed with status code: " + str(response.status_code), "response": response.text}
+    except Exception as e:
+        return {"error": "An error occurred: " + str(e)}
 #landing page
 @app.route('/')
 def home():
     return render_template('index.html')
 
 
-#chat endpoint
-@app.route('/api/chat', methods=['POST', 'GET'])
-def chat():
+#pipeline chat endpoint
+@app.route('/ai/pipeline/chat', methods=['POST', 'GET'])
+def chat_pipeline():
     json_body = request.get_json()
 
     #call helperfunction to make api call
-    response = make_post_request(json_body)
+    response = make_post_request_pipeline(json_body)
 
     #return response, just the content 
     content = response['choices'][0]['message']['content']
     return jsonify(content)
+
+
+@app.route('/openai/direct/chat', methods=['POST', 'GET'])
+def chat_openai():
+    json_body = request.get_json()
+
+    #call helperfunction to make api call
+    response = make_post_request_openai(json_body)
+
+    #return response, just the content 
+    content = response['choices'][0]['message']['content']
+    return jsonify(content)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
