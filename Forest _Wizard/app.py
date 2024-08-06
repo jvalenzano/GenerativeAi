6cfb29cc36ae30624d1085260538319894e0f16d
MIR M. ALI <Mir.Ali@usda.gov>
2024-08-06 13:54:10 -0400

    adding key

--- Forest _Wizard/app.py
+++ Forest _Wizard/app.py
@@ -4,7 +4,7 @@ import json
 import os
 
 app = Flask(__name__)
-api_key = os.getenv('OPENAI_API_KEY')
+api_key = 'e3632968ed2e48f5b1cac0b036b9affe'
 Ocp_Apim_Subscription_Key = '5bbf6c102d86484c818aba9d18588962'
 
 #####################################
