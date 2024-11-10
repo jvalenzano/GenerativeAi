9b6a74d5e46493426248364fdf5526389fcfb602
christopher.wood <christopher.wood@nrlssc.navy.mil>
2024-11-10 22:28:30 +0000

    Removed extranous content added from the past.

--- cwood3_experiments/script/set_env.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-#
-
-echo "source ~/.bashrc_lib_paths" >> ~/.bashrc;
-touch ~/.bashrc_lib_paths;
-echo "export LD_LIBRARY_PATH=\"/usr/lib:/usr/lib64:/usr/lib:/usr/lib64:~/.local/lib/python3.9/site-packages/nvidia/cudnn/lib:~/.local/lib/python3.9/site-packages/tensorrt_libs\"" >> ~/.bashrc_lib_paths;
